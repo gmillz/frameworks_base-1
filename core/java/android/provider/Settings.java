3da985951c3de9a545cc7f3d4243e1eb98db196c
cristianomatos <cristianobmatos@gmail.com>
2015-02-16 19:18:42 -0200

    [Base] Volume panel timeout
    - This feature keeps Google behavior on safe warning and zen panel expanded mode. All the others media will respect the custom timeouts.
    
    This feature is based in kk feature, from here: https://github.com/cristianomatos/android_frameworks_base/commit/0958e539f568352f0d44a29481275329d7c3a9dd. Thanks @UtkarshGupta for the idea
    
    Change-Id: I6197492bdf957cc9fa4258e1950c6cc875a4f431
    Signed-off-by: Cristiano Matos <cristianomatos@gmail.com>
    Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3559,6 +3559,12 @@ public final class Settings {
         public static final String HEADS_UP_TOUCH_OUTSIDE = "heads_up_touch_outside";
 
         /**
+         * Timeout for volume panel
+         * @hide
+         */
+        public static final String VOLUME_PANEL_TIMEOUT = "volume_panel_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
